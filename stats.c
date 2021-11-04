002527a3e094db26e31ebf4153f86b6165b13784
Kwabena Darkwa Obeng-Yeboah <darkwak@live.com>
2021-11-04 15:30:39 +0000

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,10 +1,9 @@
 /******************************************************************************
- * Copyright (C) 2021 by Alex Fosdick - University of Colorado
+ * Copyright (C) 2021 by  - Kwame Nkrumah University of Science and Technology
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
+ * forms is permitted as long as the files maintain this copyright.
+ * Kwabena Darkwa Obeng-Yeboah is not liable for any
  * misuse of this material. 
  *
  *****************************************************************************/
@@ -23,28 +22,25 @@
 
 #include <stdio.h>
 #include "stats.h"
+#include <stdlib.h>
+#include <math.h>
 
 /* Size of the Data Set */
 #define SIZE (40)
-int print_statistics();
-int print_array();
-int find_median();
-int find_mean();
-int find_maximum();
-int find_minimum();
-int sort_array();
+
 
 void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+  int test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+ sort_array(test,SIZE);
+ print_array(test,SIZE);
+ print_statistic(find_maximum(test,SIZE),find_minimum(test),find_mean(test,SIZE),find_median(test,SIZE));
+
 
 }
 
-/* Add other Implementation File Code Here */
